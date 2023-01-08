17d4b483e462b06219dee85c1351e601053bbb17
klark003 <oscar.trujillo@correounivalle.edu.co>
2023-01-08 01:45:17 -0500

    add events key game

--- js/javascript.js
+++ js/javascript.js
@@ -2,16 +2,20 @@ let gameScene = new Phaser.Scene('Game');
 
 var teclas;
 var playing = false;
+var dir = "-";
+var onAir = false;
+var distanceJump = -80;
+var isLoadJump = false;
 
-gameScene.init = function(){
+gameScene.init = function () {
 
 }
 
-gameScene.preload = function(){
+gameScene.preload = function () {
     //cargar de imagenes
-    this.load.image('floor','assets/img/floor.jpg');
-    this.load.image('block','assets/img/block.jpg');
-    this.load.image('player','assets/img/caballero.jpeg');
+    this.load.image('floor', 'assets/img/floor.jpg');
+    this.load.image('block', 'assets/img/block.jpg');
+    this.load.image('player', 'assets/img/caballero.jpeg');
     this.load.image('fondo', 'assets/img/fondo3.png');
 
     //Cargar sonido
@@ -19,31 +23,31 @@ gameScene.preload = function(){
 
 }
 
-gameScene.create = function(){
+gameScene.create = function () {
 
     if (!playing && !this.load.isLoading()) {
         playing = true
-        var music = this.sound.add('music') 
+        var music = this.sound.add('music')
         music.loop = true;
     }
 
     this.add.image(500, 380, 'fondo');
     //piso 
-    var piso = this.add.tileSprite(480,700, 3* 420, 1* 100, 'floor');
+    var piso = this.add.tileSprite(480, 700, 3 * 420, 1 * 100, 'floor');
     this.physics.add.existing(piso, true);
 
     //escalon
-    var platform = this.add.tileSprite(230,500, 5* 69, 1* 49, 'block');
+    var platform = this.add.tileSprite(230, 500, 5 * 69, 1 * 49, 'block');
     this.physics.add.existing(platform, true);
 
-    var platform2 = this.add.tileSprite(650,370, 4* 69, 2* 49, 'block');
+    var platform2 = this.add.tileSprite(650, 370, 4 * 69, 2 * 49, 'block');
     this.physics.add.existing(platform2, true);
 
-    var platform3 = this.add.tileSprite(200,200, 2* 69, 1* 49, 'block');
+    var platform3 = this.add.tileSprite(200, 200, 2 * 69, 1 * 49, 'block');
     this.physics.add.existing(platform3, true);
 
     //player
-    this.player = this.add.sprite(180,400,'player');
+    this.player = this.add.sprite(180, 462, 'player');
     this.physics.add.existing(this.player);
 
     //colisiones
@@ -57,17 +61,36 @@ gameScene.create = function(){
 
 }
 
-gameScene.update = function(){
+gameScene.update = function () {
     this.player.body.velocity.x = 0;
-    if(teclas.left.isDown){
-        this.player.body.velocity.x = -90;
-    }else if(teclas.right.isDown){
-        this.player.body.velocity.x = 90;
-
-    }if (teclas.up.isDown && this.player.body.touching.down){
-        this.player.body.velocity.y = -370;
+    if(teclas.up.isDown){
+        if(this.player.body.touching.down){
+            isLoadJump = true;
+            if(distanceJump >= -400){
+                distanceJump -= 2;
+            }
+        }
+    }else if(isLoadJump){
+        isLoadJump = false;
+        this.player.body.velocity.y = distanceJump;
     }
-    
+    if (this.player.body.touching.down) {
+        if (teclas.left.isDown) {
+            dir = "-";
+        } else if (teclas.right.isDown) {
+            dir = "+";
+        }
+        if(!teclas.up.isDown){
+            distanceJump = -80;
+        }
+    }else{
+        if (dir == "-") {
+            this.player.body.velocity.x = -200;
+        } else {
+            this.player.body.velocity.x = 200;
+        }
+    }
+
 }
 
 
@@ -77,13 +100,13 @@ const config = {
     height: 840,
     scene: gameScene,
     physics: {
-		default: 'arcade',
-		arcade: {
-			//debug: true,
-			gravity: {
-				y: 300,
-			},
-		},
+        default: 'arcade',
+        arcade: {
+            //debug: true,
+            gravity: {
+                y: 300,
+            },
+        },
     }
 };
 
